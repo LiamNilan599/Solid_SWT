e12d0f9906d6affc0b7e06739fb2d08bd892ebb5
Liam Nilan <g00366621@gmit.ie>
2022-03-29 09:55:52 +0100

    MainMessageApp now can choose which format is written to the file

--- src/main/java/ie/gmit/exercise/MainMessageApp.java
+++ src/main/java/ie/gmit/exercise/MainMessageApp.java
@@ -1,6 +1,8 @@
 package ie.gmit.exercise;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainMessageApp {
 
@@ -9,27 +11,34 @@ public class MainMessageApp {
 		Formatter formatterJ = new JSONFormatter();//creates formatter
 		TextFormatter formatterT = new TextFormatter();
 		MessagePrinter printer = new MessagePrinter();
+		String fileName = "test_msg.txt";
+
 
 		Message msg = new Message("This is a message");
 		int choice = 3;
 		if (choice == 0)
 		{
-
-			printer.writeMessage(msg, "test_msg.txt" , formatterT);
+			PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+			printer.writeMessage(msg, formatterT ,writer);
 		}
 		else if (choice == 1)
 		{
-			printer.writeMessage(msg, "test_msg.txt", formatterJ);
+			PrintWriter writer = new PrintWriter(new FileWriter(fileName));
+			printer.writeMessage(msg, formatterJ ,writer);
 		}
 		else if (choice == 2)
 		{
+			PrintWriter writer = new PrintWriter(System.out);
 			String text = formatterT.format(msg);
-			System.out.println(text);
+			printer.writeMessage(msg, formatterJ ,writer);
+			//System.out.println(text);
 		}
 		else if (choice == 3)
 		{
 			String json = formatterJ.format(msg);
-			System.out.println(json);
+			PrintWriter writer = new PrintWriter(System.out);
+			printer.writeMessage(msg, formatterJ ,writer);
+			//System.out.println(json);
 		}
 	}
 }
--- src/main/java/ie/gmit/exercise/MessagePrinter.java
+++ src/main/java/ie/gmit/exercise/MessagePrinter.java
@@ -2,19 +2,17 @@ package ie.gmit.exercise;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Writer;
 
 public class MessagePrinter {
 
 	//Writes message to a file
-	public void writeMessage(Message msg, String fileName , Formatter format) throws IOException
+	public void writeMessage(Message msg, Formatter format, PrintWriter writer) throws IOException
 	{
-		//Formatter formatter = new JSONFormatter();//creates formatter
-
-		try(PrintWriter writer = new PrintWriter(new FileWriter(fileName)))
-		{ //creates print writer
+		//creates print writer
 			writer.println(format.format(msg)); //formats and writes message
 			writer.flush();
 			writer.close();
-		}
+
 	}
 }
